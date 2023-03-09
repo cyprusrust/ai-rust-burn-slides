13ad628d9f0b07f61acc3f4cd2c3d23d13c0282e
Federico Rampazzo <frampone@gmail.com>
2023-03-09 14:31:55 +0000

    reset demo example

--- demo/src/bin/main.rs
+++ demo/src/bin/main.rs
@@ -1,22 +1,20 @@
 use std::thread;
 use std::time::Duration;
-use std::sync::Arc;
 
 fn main() {
-    let data = Arc::new(vec![1, 2, 3]);
+    //FIX ME 
 
-    let data1 = data.clone();
-    let data2 = data.clone();
+    let data = vec![1, 2, 3];
 
     let handle1 = thread::spawn(move || {
-        for i in data1.iter() {
+        for i in data.iter() {
             println!("Thread 1: {}", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
     let handle2 = thread::spawn(move || {
-        for i in data2.iter() {
+        for i in data.iter() {
             println!("Thread 2: {}", i);
             thread::sleep(Duration::from_millis(1));
         }
